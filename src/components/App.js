cf7361cb93c04e18e87dbf3f9ef616191c11e430
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2024-01-09 09:52:09 +0000

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import { event } from "cypress/types/jquery";
 
 const App = () => {
+  const[input, setInput]=useState("");
   return (
     <div>
-        <h1>Welcome to My Greeting App</h1>
-        <GreetingComponent />
+      <P>Enter your name: </P>
+      <input type="text" onChange={(event)=>setInput(event.target.value)}></input>
+
+
+      {input ? <p>Hello {input}!</p>:""}  
     </div>
-  )
-}
+  );
+};
 
 export default App
