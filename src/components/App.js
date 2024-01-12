b26e20f9dce4875f539ad795e93f9650a5f9adff
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2024-01-12 05:05:17 +0000

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
-
 import React, { useState } from "react";
 import './../styles/App.css';
-import { event } from "cypress/types/jquery";
 
 const App = () => {
   const[input, setInput]=useState("");
   return (
     <div>
-      <P>Enter your name: </P>
+      <p>Enter your name: </p>
       <input type="text" onChange={(event)=>setInput(event.target.value)}/>
-
 
       {input ? <p>Hello {input}!</p> : ""}  
     </div>
