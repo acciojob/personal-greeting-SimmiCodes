b6059b0504c0772a974246317f575f8bf6661ff8
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2024-01-12 05:11:22 +0000

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
-import './../styles/App.css';
-
 const App = () => {
-  const[input, setInput]=useState("");
+  const [input, setInput] = useState("");
+  const [name, setName] = useState("");
+
+  const handleClick = () => {
+    setName(input);
+  };
+
   return (
     <div>
       <p>Enter your name: </p>
-      <input type="text" onChange={(event)=>setInput(event.target.value)}/>
-
-      {input ? <p>Hello {input}!</p> : ""}  
+      <input type="text" onChange={(event) => setInput(event.target.value)} />
+      <button onClick={handleClick}>Greet Me</button>
+      {name ? <p>Hello {name}!</p> : ""}  
     </div>
   );
 };
-
-export default App
