6704a4edb7ad879dd7620e23689c1036f7c149cf
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2024-02-10 11:07:30 +0000

    cgs

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
-const handleClick = () => {
-  const [name, setName] = useState("");
-
-  const handleChange = (event) => {
-    setName(event.target.value);
-  };
-
+const App = () => {
+    const [name, setName] = useState("");
+  
+    const handleChange = (event) => {
+      setName(event.target.value);
+    };
   return (
     <div>
-      <p htmlFor="nameInput">Enter your name:</p>
-      <input type="text" id="nameInput" value={name} onChange={handleChange} placeholder="Your Name" />
-      {name && <p>Hello {name}!</p>}
+        {/* Do not remove the main div */}
+        <p htmlFor="nameInput">Enter your name:</p>
+        <input type="text" id="nameInput" value={name} onChange={handleChange} placeholder="Your Name" />
+        {name && <p>Hello {name}!</p>}
     </div>
-  );
-};
+  )
+}
 
-export default handleClick;
+export default App
